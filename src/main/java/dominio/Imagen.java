f7a143c361a045a009e337938911a09c184de14c
Walter Schmidt <sebastian.taka@hotmail.com>
2019-08-19 23:42:14 -0300

    Fix Imagen

--- src/main/java/dominio/Imagen.java
+++ src/main/java/dominio/Imagen.java
@@ -7,17 +7,30 @@ import java.awt.image.WritableRaster;
 import java.util.Hashtable;
 
 public class Imagen extends BufferedImage {
-
-	public Imagen(int width, int height, int imageType, IndexColorModel cm) {
+	
+	private String nombreDeArchivo;
+	
+	public Imagen(int width, int height, int imageType, IndexColorModel cm, String nombreDeArchivo) {
 		super(width, height, imageType, cm);
+		this.setNombreDeArchivo(nombreDeArchivo);
 	}
 
-	public Imagen(int width, int height, int imageType) {
+	public Imagen(int width, int height, int imageType, String nombreDeArchivo) {
 		super(width, height, imageType);
+		this.setNombreDeArchivo(nombreDeArchivo);
 	}
 
-	public Imagen(ColorModel arg0, WritableRaster arg1, boolean arg2, Hashtable<?, ?> arg3) {
+	public Imagen(ColorModel arg0, WritableRaster arg1, boolean arg2, Hashtable<?, ?> arg3, String nombreDeArchivo) {
 		super(arg0, arg1, arg2, arg3);
+		this.setNombreDeArchivo(nombreDeArchivo);
+	}
+
+	public String getNombreDeArchivo() {
+		return nombreDeArchivo;
+	}
+
+	public void setNombreDeArchivo(String nombreDeArchivo) {
+		this.nombreDeArchivo = nombreDeArchivo;
 	}
 	
 
